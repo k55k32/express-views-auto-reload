d47d090a0a627dca870d818cc061aae22e3d40f2
周超 <zhouchao@zhixueyun.com>
2016-11-24 10:33:21 +0800

    add autoreload

---
+++ index.js
@@ -0,0 +1,74 @@
+import ws from 'ws'
+import watch from 'node-watch'
+
+let clients = []
+let currentVersion  = new Date().getTime()
+
+function jsTemp (host, port) {
+  let functionName = `initWebsocket${currentVersion}`
+  return
+  `<script>
+    function ${functionName}() {
+      var ws = new window.WebSocket('ws://${host}:${port}')
+      var storeKey = "auto_reload_current_version"
+      ws.onmessage = ({ data }) => {
+        var currentVersion = data
+        var localVersion = localStorage.getItem(storeKey)
+        if (currentVersion !== localVersion) {
+          localStorage.setItem(storeKey, currentVersion)
+          location.reload()
+        }
+      }
+      ws.onclose = () => {
+        setTimeout(() => { ${functionName}() }, 300)
+      }
+    }
+    ${functionName}()
+  </script>
+  `
+}
+
+function sendToAll() {
+  clients.forEach(ws => {
+    if (ws.readyState === 1) ws.send(currentVersion)
+  })
+}
+
+function createWs(port) {
+  const wsServer = ws.Server({ port: port })
+  wsServer.on('connection', ws => {
+    clients.push(ws)
+    ws.send(currentVersion)
+    ws.on('close', () => {
+      clients.splice(clients.findIndex(item => item === ws), 1)
+    })
+  })
+}
+
+export default ({port, app, watchs = [], host = 'localhost'}) => {
+  createWs(port)
+  if (watchs.length) {
+    watch('./', filename => {
+        watchs.forEach(prefix => {
+          if (filename.endsWith(prefix)) {
+            currentVersion = new Date().getTime()
+            sendToAll()
+            return false
+          }
+        })
+    })
+  }
+
+  return (req, res, next) => {
+    res.render = (path) => {
+      app.render(path,
+        { locals: app.locals },
+        (err, str) => {
+          let send = str + jsTemp(host, port)
+          res.send(send)
+        }
+      )
+    }
+    next()
+  }
+}
